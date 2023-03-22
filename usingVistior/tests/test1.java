6a7f3f6fcae196e7c9b597c40c52fb39de24a7b0
Mostafa Emad AL -din <66144435+Mostafa-Emad77@users.noreply.github.com>
2023-03-22 02:06:30 +0200

    Uploading visitor code

---
+++ usingVistior/tests/test1.java
@@ -0,0 +1,9 @@
+public class Input {
+    public static void main(String[] args) {
+        if( x == true ) {
+            if (z  == false){
+                print();
+            }
+        }
+    }
+}
