389dd4bb88e900c676246e84b0562902af03387a
Mostafa Emad AL -din <66144435+Mostafa-Emad77@users.noreply.github.com>
2023-03-28 22:47:51 +0200

    Add files via upload

---
+++ src/input.java
@@ -0,0 +1,26 @@
+public class input {
+    public static void main(String[] args) {
+
+        int x = 3;
+        if(x==5){
+
+        }else if (x==8){
+
+        }else{
+
+        }
+
+        if(x==3){
+
+        }
+        while (x>0){
+
+            x--;
+        }
+        int y = 5;
+        for(int i = 0;i<y;i++) {
+
+        }
+    }
+}
+
